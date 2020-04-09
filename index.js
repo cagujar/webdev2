65da9e2a334fc65634b7b3c49eb9357d5f2c9c45
cagujar <cagujar_180000001508@uic.edu.ph>
2020-04-09 12:20:45 +0800

    kulang preloading

--- index.js
+++ index.js
@@ -37,6 +37,5 @@ app.get('/', (req, res)=>{
 });
 
 // listen to requests
-app.listen(3000, ()=> {
-    console.log('app now listening for requests on port 3000');
-});
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
